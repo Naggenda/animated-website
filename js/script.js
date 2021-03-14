d9446446b6b282a0f99c1ea9bdb0166750341c38
Naggenda <naggendajoshua@gmail.com>
2021-03-14 16:04:30 +0300

    js addition

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var card = document.getElementById("card")
+var card = document.getElementById("card");
 
 function openRegister(){
     card.style.transform = "rotateY(-180deg)";
@@ -7,3 +7,9 @@ function openRegister(){
 function openLogin(){
     card.style.transform = "rotateY(0)";
 }
+
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
